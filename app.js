64f1377f51deca91ae445b20ad29d199b1e8ded7
mage3k <loper.tt@gmail.com>
2017-03-30 00:22:35 +0800

    update gh-pages

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var Vue = require('vue')
 var autosize = require('./index')
 var loremIpsum = require('lorem-ipsum')
 Vue.use(autosize)
-
+Vue.config.devtools = true
 var App = new Vue({
   replace: false,
   el: '#app',
   data: function() {
     return {
+      tab: 'textarea',
       value: 'a Vue directive for autosize'
     }
   },
--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 var autosize = require('autosize')
+var autoSizeInput = require('autosize-input')
 
 exports.install = function(Vue) {
   Vue.directive('autosize', {
-    bind: function() {
-      autosize(this.el)
+    bind: function(el, binding) {
+      var tagName = el.tagName
+      if (tagName == 'TEXTAREA') {
+        autosize(el)
+      } else if (tagName == 'INPUT' && el.type == 'text') {
+        autoSizeInput(el)
+      }
     },
 
-    update: function(value) {
-      this.el.value = value
-      autosize.update(this.el)
+    componentUpdated: function(el, binding, vnode) {
+      var tagName = el.tagName
+      if (tagName == 'TEXTAREA') {
+        autosize.update(el)
+      } else if (tagName == 'INPUT' && el.type == 'text') {
+        autoSizeInput(el)
+      }
     },
 
-    unbind: function() {
-      autosize.destroy(this.el)
+    unbind: function(el) {
+      autosize.destroy(el)
     }
   })
 }
