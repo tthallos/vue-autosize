d81570d01350cead08a946a518c5dd15bfc828b3
Wyatt Arent <hello@wyatt.engineer>
2017-12-16 12:24:14 -0800

    Bugfix resize on load

--- src/index.js
+++ src/index.js
@@ -4,21 +4,25 @@ var autoSizeInput = require('autosize-input')
 exports.install = function(Vue) {
   Vue.directive('autosize', {
     bind: function(el, binding) {
-      var tagName = el.tagName
-      if (tagName == 'TEXTAREA') {
-        autosize(el)
-      } else if (tagName == 'INPUT' && el.type == 'text') {
-        autoSizeInput(el)
-      }
+      Vue.nextTick(function() {
+        var tagName = el.tagName
+        if (tagName == 'TEXTAREA') {
+          autosize(el)
+        } else if (tagName == 'INPUT' && el.type == 'text') {
+          autoSizeInput(el)
+        }
+      });
     },
 
     componentUpdated: function(el, binding, vnode) {
-      var tagName = el.tagName
-      if (tagName == 'TEXTAREA') {
-        autosize.update(el)
-      } else if (tagName == 'INPUT' && el.type == 'text') {
-        autoSizeInput(el)
-      }
+      Vue.nextTick(function() {
+        var tagName = el.tagName
+        if (tagName == 'TEXTAREA') {
+          autosize.update(el)
+        } else if (tagName == 'INPUT' && el.type == 'text') {
+          autoSizeInput(el)
+        }
+      })
     },
 
     unbind: function(el) {
